6e88729894b46c8add3e4138d76b5492462d5bd6
wangxingyuan <nova_wangxy@126.com>
2018-07-10 15:57:02 +0800

    TDD-5增加回复的表单

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -21,5 +21,26 @@
                 @endforeach
             </div>
         </div>
+
+        @if(auth()->check())
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <form action="{{$thread->path() . '/replies'}}" method="post">
+                    {{csrf_field()}}
+                    <div class="form-group">
+                        <textarea name="body" id="body" class="form-control" rows="5" placeholder="说点什么吧...."></textarea>
+                    </div>
+
+                    <button type="submit" class="btn btn-default">提交</button>
+                </form>
+
+            </div>
+        </div>
+        @else
+        <p class="text-center">
+            请先 <a href="{{route('login')}}">登陆</a>,然后再发表回复
+        </p>
+
+        @endif
     </div>
 @endsection
